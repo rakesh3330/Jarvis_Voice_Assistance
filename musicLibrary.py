1b76af1e9fa619a45c3fb2a93873d1ab1b10ab14
RAKESH KUMAR <rakeshkumargupta3330@gmail.com>
2025-10-16 14:28:05 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "wolf": "https://www.youtube.com/watch?v=BrKnJw82Lus&list=RDBrKnJw82Lus&start_radio=1",
+    "barbaad": "https://www.youtube.com/watch?v=W1y8blwMLxY&list=RDBrKnJw82Lus&index=7",
+    "skyfall": "https://www.youtube.com/watch?v=BrKnJw82Lus&list=RDBrKnJw82Lus&start_radio=1",
+    "wolf": "https://www.youtube.com/watch?v=ThCH0U6aJpU&list=PLnrGi_-oOR6wm0Vi-1OsiLiV5ePSPs9oF&index=21"
+}
